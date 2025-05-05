9bff71ce837a30c190209586a04e424fa1dbf9e6
tanzheen <ntan418@gmail.com>
2025-05-05 10:31:33 +0800

    add epub conversion

--- main.py
+++ main.py
@@ -1,25 +1,57 @@
 import subprocess
 import argparse
 import os
+import ebooklib
+from ebooklib import epub
 
-def run_marker(foldername, output_dir):
-    for file in os.listdir(foldername):
-        command = [
-            "uv", "run", "marker_single",
-            os.path.join(foldername, file),
-            "--output_dir", output_dir,
-            "--output_format", "markdown"
+# convert pdf to markdown
+def run_marker(filename, output_dir):
+    command = [
+        "uv", "run", "marker_single",
+        filename,
+        "--output_dir", output_dir,
+        "--output_format", "markdown"
     ]
 
-        try:
-            print("Running command:", " ".join(command))
-            subprocess.run(command, check=True)
+    try:
+        print("Running command:", " ".join(command))
+        subprocess.run(command, check=True)
 
-            # if succeeded, delete the files in the folder
-            os.remove(os.path.join(foldername, file))
+        # if succeeded, delete the files in the folder
+        os.remove(filename)
 
-        except subprocess.CalledProcessError as e:
-            print("Command failed:", e)
+    except subprocess.CalledProcessError as e:
+        print("Command failed:", e)
+
+
+# convert epub to markdown
+def run_epub(filename, output_dir):
+    try:
+        command = [
+            "pandoc", filename,
+            "-f", "epub",
+            "-t", "markdown",
+            "-o", os.path.join(output_dir, os.path.basename(filename) + ".md")
+        ]
+        subprocess.run(command, check=True)
+
+        # if succeeded, delete the files in the folder
+        os.remove(filename)
+
+    except subprocess.CalledProcessError as e:
+        print("Command failed:", e)
+    
+
+
+def run_main(foldername, output_dir):
+    for file in os.listdir(foldername):
+        print("Processing file: {0}".format(file))
+        if file.endswith(".pdf"):
+            run_marker(os.path.join(foldername, file), output_dir)
+        elif file.endswith(".epub"):
+            run_epub(os.path.join(foldername, file), output_dir)
+        else:
+            print("Unknown file type: {0}".format(file))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Wrapper for `uv run marker`.")
@@ -27,4 +59,4 @@ if __name__ == "__main__":
     parser.add_argument("--output_dir", required = False, default="./conversion_results", help="Output directory")
 
     args = parser.parse_args()
-    run_marker(args.foldername, args.output_dir)
+    run_main(args.foldername, args.output_dir)
